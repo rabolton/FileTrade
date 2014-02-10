d2b36ac5602a86d476d3955179fcd40bb85cc1e4
rabolton <rob.a.bolton@gmail.com>
2014-02-10 13:33:30 +0000

    Setup project. Created and populated device search activity

---
+++ SRC/src/uk/co/rob_bolton/filetrade/FindDevices.java
@@ -0,0 +1,22 @@
+package uk.co.rob_bolton.filetrade;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FindDevices extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_find_devices);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.find_devices, menu);
+		return true;
+	}
+
+}
